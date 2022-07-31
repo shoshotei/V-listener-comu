f1b991fbc45e764f592f583027f014679977c8c6
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-07-31 08:22:33 +0000

    [Add]退会機能の追加

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -8,4 +8,20 @@ class Admin::EndUsersController < ApplicationController
     @end_user = EndUser.find(params[:id])
     @submissions = @end_user.submissions
   end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    @end_user.update(end_user_params)
+    redirect_to admin_end_user_path(@end_user.id)
+  end
+
+  private
+
+  def end_user_params
+   params.require(:end_user).permit(:id, :name, :introduction, :is_deleted)
+  end
 end
--- app/controllers/end_user/sessions_controller.rb
+++ app/controllers/end_user/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class EndUser::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+    before_action :end_user_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,10 +18,20 @@ class EndUser::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
+  protected
+ def end_user_state
+  ## 【処理内容1】 入力されたemailからアカウントを1件取得
+  @end_user = EndUser.find_by(email: params[:end_user][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+  return if !@end_user
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+  if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.is_deleted == true)
+    ## 【処理内容3】
+   redirect_to new_end_user_registration_path
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+ end
 end
